bca8275ef0521d180a4fecf664a7593da58cd0c1
Github-zihao <1354609232@qq.com>
2019-10-26 09:35:38 +0800

    主人很懒，什么都没留下

--- work7side/index.js
+++ work7side/index.js
@@ -2,19 +2,26 @@ function sort(method) {
     //读取待排序数据
     let origin = document.getElementById('origin').value
     let logs
-    console.log(method)
+    // console.log(method)
     //根据传入排序的方法调用不同的排序函数
-    if (method == 'bubble') {
+    if (method ==='bubble') {
         //调用bubbleSort()冒号排序函数获取排序结果，
         logs = bubbleSort(origin)
-    } else if (method == 'insert') {
+        //console.log(123)
+        //var arr = origin.split(',')
+    } else if (method ==='insert') {
         //调用insertSort()插入排序函数获取排序结果，
         logs = insertSort(origin)
+        //console.log(333)
     }
     //获取显示冒泡排序的div
     showDiv = document.getElementById('Anim')
-    //生成排序数组的遍历器对象
     logValues = logs.values()
+    console.log(logs)
+
+    //生成排序数组的遍历器对象
+    //logValues = logs.values()
+    console.log(logValues)
     //尝试看一下logValues.next() 遍历器对象放回的数据结构
     console.log(logValues.next())
     //延时方法调用showing()函数显示排序步骤，50毫秒后调用，调用一次显示一次排序状态
--- work7side/utils.js
+++ work7side/utils.js
@@ -1,5 +1,5 @@
 //冒泡排序
-function bubbleSort(str) {
+/*function bubbleSort(str) {
     //把字符串切分成数组
     let arr = str.split(',')
     //声明排序步骤记录数组sortLog数组用于动画演示
@@ -17,7 +17,7 @@ function bubbleSort(str) {
                 //使用解构赋值的方法，把两个元素交换位置
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
                 //把比较之后的数组状态和定位指针保存到sortLog中
-                sortLog.push([arr.cancat(), [j, j + 1]])
+                sortLog.push([arr.concat(), [j, j + 1]])
             }
         }
     }
@@ -26,72 +26,97 @@ function bubbleSort(str) {
     //调试时请认真分析sortLog的数据解构
     //console.log(sortLog)
     return soreLog
+}*/
+function bubbleSort(str) {
+    let arr = str.split(',')
+    let sortLog = []
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+                sortLog.push([arr.concat(), [j, j + 1]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(), [-1, -1]])
+    return sortLog
 }
+
+function insertSort(str) {
+    let arr = str.split(',')
+    let sortLog = []
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i; j > 0; j--) {
+            sortLog.push([arr.concat(), [j - 1, j]])
+            sortLog.push([arr.concat(), [j - 1, j]])
+            sortLog.push([arr.concat(), [j - 1, j]])
+
+            if (arr[j - 1] > arr[j]) {
+                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
+                sortLog.push([arr.concat(), [j - 1, j]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(), [-1, -1]])
+    return sortLog
+}
+// function showLog(logValues,showElement) {
+//     let str = ''
+//     let {done,value:[row,pos]}=logValues.next()
+//     for(const key in row){
+//         let color = pos.indexOf(Number(key))>-1?'color:red;':''
+//         str += '<span style="font-size: '+row[key]*20+'px;'+ color+
+//             '">'+ row[key] +'</span>'
+//     }
+
+//     showElement.innerHTML=str
+//     if(pos[0]!==-1)
+//         setTimeout("showLog(logValues,showDiv)",500)
+//}
 //显示排序的每一个步骤
-function showLog(logValues, showElement) {
+//function showLog(str) {原来
+function showLog(logValues,showElement) {
     let str = ''
     //解构赋值的方法，从排序日志便利器去取出一条结果，赋值给row和pos变量
-    let {
-        done,
-        value: [row, pos]
-    } = logValues.next()
+    let {done,value:[row,pos]}=logValues.next()
     //对比一下row和pos的值是否于排序是保存一致
     //console.log(row)
     //console.log(pos)
     //遍历row数组，构建显示当前排序状态的html代码
-    for (const key in row) {
+    for(const key in row){
         //根据定位指针数据把进行比较的数字编程红色
-        let color = pos.indexOf(Number(key)) > -1 ? 'colour:red;' : ''
+        let color = pos.indexOf(Number(key))>-1?'color:red;' : ''
         //设置字体的大小等HTML代码
-        str += '<span style="font-size:' + row[key] * row[key] * 20 + 'px;' +
-            color + '">' + row[key] + '</span>'
+        // str += '<span style="font-size:' + row[key] * row[key] * 20 + 'px;' +
+        //     color + '">' + row[key] + '</span>'
+        str += '<span style="font-size: ' + row[key] * 20 + 'px;' + color +
+            '">' + row[key] + '</span>'
     }
+
+    // function showLog(logValues,showElement){
+    //     let str = ''
+    //     let{done,value:[row,pos]}=logValues.next()
+    //     for(const key in row){
+    //         let color = pos.indexOF(Number(key))>-1?'color:red':''
+    //         str += '<span style="font-size:'+row[key]*20+'px;'+color+
+    //         '">'+row[key]+'</span>'
+    //     }
+
+    // }
+
     //把html代码设置到显示的div中
     showElement.innerHTML = str
-    /*如果定位指针不是-1 （-1是最后一条记录），就调用延时方法调用
-    slideShow函数显示下一条记录，以形成动画效果*/
-    if (pos[0] != -1)
+    // 如果定位指针不是-1 （-1是最后一条记录），就调用延时方法调用
+    // slideShow函数显示下一条记录，以形成动画效果
+    if (pos[0] !== -1)
         setTimeout("showLog(logValues,showDiv)", 500)
 }
 
 //插入排序
-function insertSort(str) {
-    //老师能伴我们的只到这里了 剩下的要靠自己
+// function insertSort(str) {
+// }
 
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*前任
-//插入排序
-function insertSort(str) {
-    let arr = str.split(',')
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = i; j > 0; j--) {
-            if (arr[j - 1] > arr[j]) {
-                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]]
-            }
-        }
-    }
-    return arr
-}
-*/
